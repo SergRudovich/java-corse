0c14611901a37274d82dcf078c6b18b77a41c3fd
SergRudovich <slunrew@tut.by>
2017-04-28 14:05:19 +0300

    new calc

---
+++ calc.java
@@ -0,0 +1,12 @@
+public class calc{
+
+	public static void main(String[] arg){
+		System.out.println("calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("result ="+sum);
+
+	}
+
+}
